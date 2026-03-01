ba71841cdcdb1236cc1f6bb85c8e8f1f47d60a5c
Biswajit Rout <routhappy9876@gmail.com>
2026-03-01 15:31:19 +0530

    Initial backend setup with Express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+const app = express();      //CREATE SERVER
+
+// IF USER REQUEST THEN THIS COMES ON RESPONSE
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.get('/about', (req, res) => {
+    res.send('About Page')
+})
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+})            // START SERVER
