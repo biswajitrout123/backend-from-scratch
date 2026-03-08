06a6a771c00f28588ea7cb249484908775ebe865
Biswajit Rout <routhappy9876@gmail.com>
2026-03-08 07:07:03 +0530

    fresh start of backend

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-// TO START THE SERVER
-const app = require('./src/app')
-
-
-
+const app = require("./src/app");
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server is running on port 3000");
 })
--- src/app.js
+++ src/app.js
@@ -1,47 +1,5 @@
-// TO CREATE SERVER
-const express = require('express')
+const express = require('express');
 
-const app = express()
-app.use(express.json())     //MIDDLE WARE //“If request body contains JSON data, convert it into a JavaScript object.”
-
-const notes = []
-
-
-app.post('/notes', (req, res) => {
-    notes.push(req.body);
-
-    res.status(201).json({
-        message: "note created successfully"
-    })
-})
-
-app.get('/notes', (req, res) => {
-    res.status(200).json({
-        message: "note fetched successfully",
-        note: notes                  //“Send the notes array to the client with the name ‘notes’.”
-    })
-})
-
-app.patch('/notes/:index', (req, res) => {
-    const index = req.params.index
-    const description = req.body.description
-    
-    notes[index].description = description
-
-    res.status(200).json({
-        message: "note updated successfully"
-    })
-})
-
-app.delete('/notes/:index', (req, res) => {
-    const index = req.params.index
-
-    delete notes[index]
-
-    res.status(200).json({
-        message: "note deleted successfully"
-    })
-})
-
+const app = express();
 
 module.exports = app
