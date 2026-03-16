b5d333854424cb4323000de6ac0de61a6c1c8d10
Biswajit Rout <routhappy9876@gmail.com>
2026-03-16 07:48:31 +0530

    doing authentication and learning from yt

--- Authentication/src/controllers/auth.controller.js
+++ Authentication/src/controllers/auth.controller.js
@@ -1,9 +1,35 @@
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req, res) {
     const { username, email, password } = req.body;
 
+    /*
 
+     const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+        return res.status(400).json({
+            message: "Email already registered"
+        });
+    }
+
+    */
+
+    const user = await userModel.create({
+        username, email, password
+    })
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User register successfully",
+        user
+    })
 }
 
 module.exports = { registerUser }
