2425cac872332eaaa1b5008602a0c9302ec5796e
Biswajit Rout <routhappy9876@gmail.com>
2026-03-24 16:40:44 +0530

    Updates and done in Music Album routes in Spotify Backend Learning

--- Spotify_Clone_Authentication/src/controllers/music.controller.js
+++ Spotify_Clone_Authentication/src/controllers/music.controller.js
@@ -1,4 +1,5 @@
 const musicModel = require("../models/music.model");
+const albumModel = require("../models/album.model");
 const jwt = require("jsonwebtoken");
 const { uploadFile } = require("../services/storage.service");
 
@@ -22,43 +23,83 @@ async function createMusic(req, res) {
             })
         }
 
-    
 
-    const { title } = req.body;
-    const file = req.file
 
-    const result = await uploadFile(file.buffer.toString('base64'))
+        const { title } = req.body;
+        const file = req.file
 
-    const music = await musicModel.create({
-        uri: result.url,
-        title,
-        artist: decoded.id,
-    })
+        const result = await uploadFile(file.buffer.toString('base64'))
 
-    res.status(201).json({
-        message: "Music Created Successfully",
-        music: {
-            id: music._id,
-            uri: music.uri,
-            title: music.title,
-            artist: music.artist,
+        const music = await musicModel.create({
+            uri: result.url,
+            title,
+            artist: decoded.id,
+        })
 
-        }
-    })
+        res.status(201).json({
+            message: "Music Created Successfully",
+            music: {
+                id: music._id,
+                uri: music.uri,
+                title: music.title,
+                artist: music.artist,
+
+            }
+        })
 
     } catch (error) {
 
         console.log(error);
-        
+
         return res.status(401).json({
             message: "Unauthorized"
         })
     }
 }
 
-module.exports = { createMusic };
+async function createAlbum(req, res) {
+    const token = req.cookies.token;
 
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
 
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "You don't have access to create an album"
+            })
+        }
 
+        const { title, musicIds } = req.body;
 
+        const album = await albumModel.create({
+            title,
+            artist: decoded.id,
+            musics: musicIds,
+        })
+
+        res.status(201).json({
+            message: "Album created successfully",
+            album: {
+                id: album._id,
+                title: album.title,
+                artist: album.artist,
+                music: album.musics
+            }
+        })
+
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+}
+
+module.exports = { createMusic, createAlbum};
--- Spotify_Clone_Authentication/src/routes/music.routes.js
+++ Spotify_Clone_Authentication/src/routes/music.routes.js
@@ -11,6 +11,8 @@ const router = express.Router();
 
 router.post("/upload", upload.single("music"), musicController.createMusic)
 
+router.post("/album", musicController.createAlbum)
+
 module.exports = router;
 
 
