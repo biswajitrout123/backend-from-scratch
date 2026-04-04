b6df6225c01dff1338df3c8cde35fb4423904390
Biswajit Rout <routhappy9876@gmail.com>
2026-04-04 22:10:40 +0530

    Learned Jest

--- Jest_SuperTest/src/app.js
+++ Jest_SuperTest/src/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const validationsRules = require('./middlewares/validation.middleware');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Hello World!"
-    });
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         message: "Hello World!"
+//     });
+// });
+
+app.post('/register', validationsRules.registerUserValidationRules, (req, res) => {
+    const { username, email, password } = req.body;
+
+    res.status(201).json({
+        message: 'User registered successfully', user: { username, email }
+    })
+
+
+})
 
 module.exports = app;
